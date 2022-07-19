e483d1985bf2ec7062409b6c8c8a607949586594
jhno96 <53774255+SnowFlower96@users.noreply.github.com>
2022-07-19 17:01:32 +0900

    Modify UserServiceImpl.java
    Add @Transactional annotation

--- backend-java/src/main/java/com/ssafy/api/service/UserServiceImpl.java
+++ backend-java/src/main/java/com/ssafy/api/service/UserServiceImpl.java
@@ -11,6 +11,7 @@ import com.ssafy.api.request.UserRegisterPostReq;
 import com.ssafy.db.entity.User;
 import com.ssafy.db.repository.UserRepository;
 import com.ssafy.db.repository.UserRepositorySupport;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *	유저 관련 비즈니스 로직 처리를 위한 서비스 구현 정의.
@@ -33,6 +34,7 @@ public class UserServiceImpl implements UserService {
 	PasswordEncoder passwordEncoder;
 	
 	@Override
+	@Transactional
 	public User createUser(UserRegisterPostReq userRegisterInfo) {
 		User user = new User();
 		UserStat userStat = new UserStat();
