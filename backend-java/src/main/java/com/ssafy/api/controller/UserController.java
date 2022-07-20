250d653ba908d4033839c8602c838b613341c2ed
jhno96 <53774255+SnowFlower96@users.noreply.github.com>
2022-07-20 16:43:31 +0900

    Add password verify on UserController

--- backend-java/src/main/java/com/ssafy/api/controller/UserController.java
+++ backend-java/src/main/java/com/ssafy/api/controller/UserController.java
@@ -81,4 +81,28 @@ public class UserController {
 		return ResponseEntity.status(401).body(UserLoginPostRes.of(401, "Invalid Password", null));
 	}
 
+	@PostMapping("/verify")
+	@ApiOperation(value = "비밀번호 확인", notes = "<strong>비밀번호</strong>를 통해 본인 인증을 한다.")
+	@ApiResponses({
+			@ApiResponse(code = 200, message = "성공", response = UserLoginPostRes.class),
+			@ApiResponse(code = 401, message = "인증 실패", response = BaseResponseBody.class),
+			@ApiResponse(code = 404, message = "사용자 없음", response = BaseResponseBody.class),
+			@ApiResponse(code = 500, message = "서버 오류", response = BaseResponseBody.class)
+	})
+	public ResponseEntity<UserLoginPostRes> checkPwd(@ApiIgnore Authentication authentication, @RequestBody @ApiParam(value="패스워드", required = true) UserLoginPostReq userLoginInfo) {
+		SsafyUserDetails userDetails = (SsafyUserDetails)authentication.getDetails();
+		String userId = userDetails.getUsername();
+		String pwd = userLoginInfo.getPwd();
+
+		User user = userService.getUserById(Long.parseLong(userId));
+
+		// 로그인 요청한 유저로부터 입력된 패스워드 와 디비에 저장된 유저의 암호화된 패스워드가 같은지 확인.(유효한 패스워드인지 여부 확인)
+		if(passwordEncoder.matches(pwd, user.getPwd())) {
+			// 유효한 패스워드가 맞는 경우, 로그인 성공으로 응답.(액세스 토큰을 포함하여 응답값 전달)
+			return ResponseEntity.ok(UserLoginPostRes.of(200, "Success", JwtTokenUtil.getToken(String.valueOf(user.getId()))));
+		}
+		// 유효하지 않는 패스워드인 경우, 로그인 실패로 응답.
+		return ResponseEntity.status(401).body(UserLoginPostRes.of(401, "Invalid Password", null));
+	}
+
 }
