b73eb6d7671a87227cf3382a1511ba1d16ff29dd
jhno96 <53774255+SnowFlower96@users.noreply.github.com>
2022-07-21 17:48:27 +0900

    Revert "Modify UserController.java"
    This reverts commit bb911355541d5565892d7027376c0b744a3a668b.

--- backend-java/src/main/java/com/ssafy/api/controller/UserController.java
+++ backend-java/src/main/java/com/ssafy/api/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.ssafy.api.controller;
 
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.api.response.JsonRes;
@@ -31,7 +29,6 @@ import io.swagger.annotations.ApiResponses;
 import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.List;
-import java.util.Map;
 
 /**
  * 유저 관련 API 요청 처리를 위한 컨트롤러 정의.
@@ -49,7 +46,7 @@ public class UserController {
 	ObjectMapper mapper;
 
 	@PostMapping()
-	@ApiOperation(value = "회원 가입", notes = "<strong>아이디와 패스워드</strong>를 통해 회원가입 한다.")
+	@ApiOperation(value = "회원 가입", notes = "<strong>아이디와 패스워드</strong>를 통해 회원가입 한다.") 
     @ApiResponses({
         @ApiResponse(code = 200, message = "성공"),
         @ApiResponse(code = 401, message = "인증 실패"),
@@ -58,10 +55,10 @@ public class UserController {
     })
 	public ResponseEntity<? extends BaseResponseBody> register(
 			@RequestBody @ApiParam(value="회원가입 정보", required = true) UserRegisterPostReq registerInfo) {
-
+		
 		//임의로 리턴된 User 인스턴스. 현재 코드는 회원 가입 성공 여부만 판단하기 때문에 굳이 Insert 된 유저 정보를 응답하지 않음.
 		User user = userService.createUser(registerInfo);
-
+		
 		return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
 	}
 
@@ -82,10 +79,7 @@ public class UserController {
 		// 로그인 요청한 유저로부터 입력된 패스워드 와 디비에 저장된 유저의 암호화된 패스워드가 같은지 확인.(유효한 패스워드인지 여부 확인)
 		if(passwordEncoder.matches(password, user.getPwd())) {
 			// 유효한 패스워드가 맞는 경우, 로그인 성공으로 응답.(액세스 토큰을 포함하여 응답값 전달)
-			String accessToken = JwtTokenUtil.getAccessToken(String.valueOf(user.getId()));
-			String refreshToken = JwtTokenUtil.getRefreshToken(String.valueOf(user.getId()));
-			userService.setRefresh(user.getId(),refreshToken);
-			return ResponseEntity.ok(UserLoginPostRes.of(200, "Success", accessToken, refreshToken));
+			return ResponseEntity.ok(UserLoginPostRes.of(200, "Success", JwtTokenUtil.getToken(String.valueOf(user.getId()))));
 		}
 		// 유효하지 않는 패스워드인 경우, 로그인 실패로 응답.
 		return ResponseEntity.status(401).body(UserLoginPostRes.of(401, "Invalid Password", null));
@@ -108,50 +102,13 @@ public class UserController {
 
 		// 로그인 요청한 유저로부터 입력된 패스워드 와 디비에 저장된 유저의 암호화된 패스워드가 같은지 확인.(유효한 패스워드인지 여부 확인)
 		if(passwordEncoder.matches(pwd, user.getPwd())) {
-			String accessToken = JwtTokenUtil.getAccessToken(String.valueOf(user.getId()));
-			String refreshToken = JwtTokenUtil.getRefreshToken(String.valueOf(user.getId()));
-
-			// 유효한 패스워드가 맞는 경우, 로그인 성공으로 응답.(액세스 토큰, 리프레시 토큰을 포함하여 응답값 전달)
-			return ResponseEntity.ok(UserLoginPostRes.of(200, "Success", accessToken, refreshToken));
+			// 유효한 패스워드가 맞는 경우, 로그인 성공으로 응답.(액세스 토큰을 포함하여 응답값 전달)
+			return ResponseEntity.ok(UserLoginPostRes.of(200, "Success", JwtTokenUtil.getToken(String.valueOf(user.getId()))));
 		}
 		// 유효하지 않는 패스워드인 경우, 로그인 실패로 응답.
-		return ResponseEntity.status(401).body(UserLoginPostRes.of(401, "Invalid Password", null, null));
+		return ResponseEntity.status(401).body(UserLoginPostRes.of(401, "Invalid Password", null));
 	}
 
-	@PostMapping("/refresh")
-	@ApiOperation(value = "access토큰 재발급 요청", notes = "refresh token으로 <strong>access token</strong>을 요청한다.")
-	@ApiResponses({
-			@ApiResponse(code = 200, message = "성공", response = UserLoginPostRes.class),
-			@ApiResponse(code = 401, message = "인증 실패", response = BaseResponseBody.class),
-			@ApiResponse(code = 404, message = "사용자 없음", response = BaseResponseBody.class),
-			@ApiResponse(code = 500, message = "서버 오류", response = BaseResponseBody.class)
-	})
-	public ResponseEntity<UserLoginPostRes> refresToken(@ApiIgnore Authentication authentication, @RequestBody @ApiParam(value="리프레시 토큰", required = true) Map<String ,String > tokens) {
-
-		String accessToken = tokens.get("accessToken");
-		String refreshToken = tokens.get("refreshToken");
-		if (refreshToken != null) {
-			// parse the token and validate it (decode)
-			JWTVerifier verifier = JwtTokenUtil.getRefreshVerifier();
-
-			try {
-				DecodedJWT decodedJWT = verifier.verify(refreshToken);
-				String id = decodedJWT.getSubject();
-				String localRefreshToken = userService.getRefresh(Long.parseLong(id));
-
-				if (refreshToken.equals(localRefreshToken)) {
-					accessToken = JwtTokenUtil.getAccessToken(String.valueOf(id));
-					return ResponseEntity.ok(UserLoginPostRes.of(200, "Success", accessToken, refreshToken));
-				}
-			}catch (Exception e){
-				return ResponseEntity.status(401).body(UserLoginPostRes.of(401, "invalid refresh token", null, null));
-			}
-
-
-		}
-		return ResponseEntity.status(401).body(UserLoginPostRes.of(401, "invalid refresh token", null, null));
-	}
-	
 	/**
 	 * 요청 헤더 액세스 토큰이 포함된 경우에만 실행되는 인증 처리이후, 리턴되는 인증 정보 객체(authentication) 통해서 요청한 유저 식별.
 	 * 액세스 토큰이 없이 요청하는 경우, 403 에러({"error": "Forbidden", "message": "Access Denied"}) 발생.
