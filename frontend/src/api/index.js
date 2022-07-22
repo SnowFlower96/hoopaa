d17dc781508a188d727aea688d45d4440aa32297
wjdgus9596 <wjdgus9596@naver.com>
2022-07-22 18:02:47 +0900

    S07P12B302-55 api 연결해서 state에 올리기

---
+++ frontend/src/api/index.js
@@ -0,0 +1,16 @@
+// axios 객체를 만들거야
+import axios from 'axios'
+
+function createApi() {
+  return axios.create({
+    // 설정하는 정보가 여러개라서 객체로 지정
+    baseURL: process.env.VUE_APP_API_SERVER,
+    headers: {
+      // json 형태로 날릴거야
+      "Content-Type": "application/json",
+      "access-token" : sessionStorage.getItem("access-token")
+    },
+  });
+}
+
+export { createApi }
---
+++ frontend/src/store/index.js
@@ -0,0 +1,35 @@
+import { createApi } from "@/api";
+import Vue from 'vue';
+import Vuex from "vuex";
+import router from "@/common/lib/vue-router";
+import createPersistedState from "vuex-persistedstate";
+
+//Vue.use(Vuex);
+
+const api = createApi();
+
+export default new Vuex.Store({
+  plugins: [createPersistedState()],
+  state: {
+    isLogin: false,
+    roomList : [],
+  },
+
+  mutations : {
+    GET_ROOM_LIST(state, data) {
+      state.roomList = data;
+    }
+  },
+
+  actions : {
+    getRoomInfo({commit}) {
+      api({
+        url: `/api/v1/list/all`,
+        method: "GET",
+      }).then((res) => {
+        commit("GET_ROOM_LIST", res.data);
+      })
+    },
+
+  }
+})
