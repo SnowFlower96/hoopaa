2ba961a76efd4198bb427b3cbd044387cc1fbfcd
Jay <8x15yz@gmail.com>
2022-07-28 16:57:11 +0900

    fixed

--- frontend/src/views/home/home.vue
+++ frontend/src/views/home/home.vue
@@ -11,7 +11,7 @@
               <li id="logo">
                 <div>카테고리</div>
               </li>
-              <li v-for="(item, index) in menus" :key="index" @click="goCate(item.path)">{{item.name}}</li>
+              <li v-for="(item, index) in menus" :key="index">{{item.name}}</li>
             </ul>
           </div>
 
@@ -19,40 +19,23 @@
           <hr/>
           <div class="list">
             <div>여기는 핫 한 영상</div>
-            <!-- <el-carousel :interval="4000" type="card" height="200px">
-    <el-carousel-item v-for="item in 6" :key="item">
-      <h3 text="2xl" justify="center">{{ item }}</h3>
-    </el-carousel-item>
-            </el-carousel> -->
+            <carousel :items-to-show="1.5">
+    <slide v-for="slide in 10" :key="slide">
+      {{ roomList.json }}
+    </slide>
+
+    <template #addons>
+      <navigation />
+      <pagination />
+    </template>
+  </carousel>
             <div>
               <span>카테고리</span>
-              <div class="flex flex-wrap items-center">
-    <el-dropdown>
-      <el-button type="primary">
-        정렬조건<el-icon class="el-icon--right"><arrow-down /></el-icon>
-      </el-button>
-      <template #dropdown>
-        <el-dropdown-menu>
-          <el-dropdown-item @click="sortRecent">최신순</el-dropdown-item>
-          <el-dropdown-item @click="sortCur">참여자순</el-dropdown-item>
-          <el-dropdown-item @click="sortOld">오래된순</el-dropdown-item>
-          <el-dropdown-item @click="sortRank">랭킹순</el-dropdown-item>
-        </el-dropdown-menu>
-      </template>
-    </el-dropdown>
-      </div>
-              <el-checkbox>모집중만 보기</el-checkbox>
+              <button>정렬조건</button>
+              <el-checkbo~x>모집중만 보기</el-checkbox>
             </div>
-            <div class="list">여기가 기본 all</div>
-            <ul class="room-ul">
-              <li v-for="(room, index) in roomList" :key="index">
-              <span>{{phase[room.phase]}}</span>
-              <el-card class="room-card"><img :src="require(`@/assets/images/room.jpg`)" alt="" class="room-info"/>
-              <span>{{room.title}} <br></span>
-              <span>{{room.subtitle}}</span>
-              </el-card>
-              </li>
-            </ul>
+            <div>여기가 기본 all</div>
+
           </div>
         </div>
       </div>
@@ -60,7 +43,7 @@
   <!-- </div> -->
 </template>
 
-<style scoped>
+<style>
 
 .list-page-black-space {
   height: 100px;
@@ -95,63 +78,20 @@ ul {
 
 .main-container {
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   text-align: center;
   padding: 10px;
-  max-width: 100%;
-  flex-wrap: wrap;
-}
-.el-carousel__item h3 {
-  color: #475669;
-  opacity: 0.75;
-  line-height: 200px;
-  margin: 0;
-  text-align: center;
-}
-.el-carousel__item:nth-child(2n) {
-  background-color: #99a9bf;
 }
 
-.el-carousel__item:nth-child(2n + 1) {
-  background-color: #d3dce6;
-}
-
-.room-card {
-  max-width: 400px;
-  height: 400px;
-}
-.room-ul {
-  display: flex;
-  flex-direction: row;
-  overflow: auto;
-  list-style: none;
-}
-.list {
-  max-width: inherit;
-}
-.room-info {
-  width: 300px;
-  height: 200px;
-}
-.example-showcase .el-dropdown + .el-dropdown {
-  margin-left: 15px;
-}
-.example-showcase .el-dropdown-link {
-  cursor: pointer;
-  color: var(--el-color-primary);
-  display: flex;
-  align-items: center;
-}
 </style>
-<script >
+<script>
 import Search from '@/views/common/search'
 import Conference from './components/conference'
 import 'vue3-carousel/dist/carousel.css';
 import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
 import { reactive } from 'vue'
 import { useRouter } from 'vue-router'
-import { removeComments } from '@babel/types';
-import { mapState , mapMutations} from "vuex";
+import { mapState } from "vuex";
 
 export default {
   name: 'Home',
@@ -160,44 +100,14 @@ export default {
     Conference,
     Search,
   },
-  data () {
-    return {
-      phase : {0:"모집중", 1:"진행중", 3:"투표중", 4:"종료"},
-      menus : '',
-    }
-  },
   computed : {
-    ...mapState(["roomList"])
+    ...mapState(["roomList", "menus"])
   },
   created() {
     this.$store.dispatch("getRoomInfo");
-    const menuData = require('@/views/main/menu.json')
-    this.menus = menuData;
   },
    methods: {
-    ...mapMutations(["GET_ROOM_LIST"]),
-    goCate(index)  {
-      console.log(index)
-      this.$store.dispatch("getRoomInfoCate", index);
-    },
-    sortRecent () {
-      const orderedDate = this.$store.state.roomList.sort((a, b) => new Date(b.start_time) - new Date(a.start_time))
-      this.GET_ROOM_LIST(orderedDate);
-      this.$router.replace;
-    },
-    sortCur () {
-      const sortdata = this.$store.state.roomList.sort((a,b) => b.cur_num - a.cur_num);
-      this.GET_ROOM_LIST(sortdata);
-      this.$router.replace;
-    },
-    sortOld () {
-      const orderedDate = this.$store.state.roomList.sort((a, b) => new Date(a.start_time) - new Date(b.start_time))
-      this.GET_ROOM_LIST(orderedDate);
-      this.$router.replace;
-    },
-    sortRank () {
 
-    }
   },
   // setup () {
   //   const router = useRouter()
