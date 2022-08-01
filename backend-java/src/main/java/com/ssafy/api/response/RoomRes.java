d62dcf906566ed52a69f2f9c7c82e21af2325e12
SSAFY <wj5295@naver.com>
2022-08-02 00:52:17 +0900

    Modify RoomRes.java
    fix: 토론 방 응답 데이터 형식 수정

--- backend-java/src/main/java/com/ssafy/api/response/RoomRes.java
+++ backend-java/src/main/java/com/ssafy/api/response/RoomRes.java
@@ -2,6 +2,7 @@ package com.ssafy.api.response;
 
 import com.ssafy.api.service.RoomService;
 import com.ssafy.common.model.response.BaseResponseBody;
+import com.ssafy.db.dto.RoomInfoDto;
 import com.ssafy.db.entity.RoomInfo;
 import io.swagger.annotations.ApiModel;
 import lombok.Getter;
@@ -23,21 +24,23 @@ public class RoomRes extends BaseResponseBody {
     LocalDateTime start_time;
     String title;
     String subtitle;
+    String nnm;
 
-    public static RoomRes of(Integer statusCode, String message, RoomInfo roomInfo) {
+    public static RoomRes of(Integer statusCode, String message, RoomInfoDto  roomInfoDto) {
         RoomRes res = new RoomRes();
         res.setStatusCode(statusCode);
         res.setMessage(message);
-        res.setId(roomInfo.getId());
-        res.setHost_id(roomInfo.getHost_id());
-        res.setIs_sys(roomInfo.getIs_sys());
-        res.setThumb_url(roomInfo.getThumb_url());
-        res.setPhase(roomInfo.getPhase());
-        res.setMax_num(roomInfo.getMax_num());
-        res.setCur_num(roomInfo.getCur_num());
-        res.setStart_time(roomInfo.getStart_time());
-        res.setTitle(roomInfo.getTitle());
-        res.setSubtitle(roomInfo.getSubtitle());
+        res.setId(roomInfoDto.getId());
+        res.setHost_id(roomInfoDto.getHost_id());
+        res.setIs_sys(roomInfoDto.getIs_sys());
+        res.setThumb_url(roomInfoDto.getThumb_url());
+        res.setPhase(roomInfoDto.getPhase());
+        res.setMax_num(roomInfoDto.getMax_num());
+        res.setCur_num(roomInfoDto.getCur_num());
+        res.setStart_time(roomInfoDto.getStart_time());
+        res.setTitle(roomInfoDto.getTitle());
+        res.setSubtitle(roomInfoDto.getSubtitle());
+        res.setNnm(roomInfoDto.getNnm());
 
         return res;
     }
