24b62b3df86fa79681c982ceebe9b4513b01e52d
SSAFY <wj5295@naver.com>
2022-08-01 11:40:32 +0900

    Modify RoomController.java
    feat: 방 생성 시 방 정보 반환

--- backend-java/src/main/java/com/ssafy/api/controller/RoomController.java
+++ backend-java/src/main/java/com/ssafy/api/controller/RoomController.java
@@ -1,14 +1,13 @@
 package com.ssafy.api.controller;
 
 import com.ssafy.api.request.RoomOpenReq;
+import com.ssafy.api.response.RoomRes;
 import com.ssafy.api.service.RoomService;
 import com.ssafy.common.model.response.BaseResponseBody;
+import com.ssafy.db.entity.RoomInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/v1/room")
 @RestController
@@ -21,8 +20,15 @@ public class RoomController {
     @PostMapping()
     public ResponseEntity<? extends BaseResponseBody> openRoom(@RequestBody RoomOpenReq openInfo){
 
-        roomService.openRoom(openInfo);
-
-        return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
+        RoomInfo roomInfo = roomService.openRoom(openInfo);
+        System.out.println(roomInfo.getId());
+        return ResponseEntity.status(200).body(RoomRes.of(200, "Success",roomInfo));
     }
+
+    @PutMapping()
+    public ResponseEntity<? extends BaseResponseBody> startRoom(@RequestParam int room_id){
+        return null;
+    }
+
+
 }
