b7e967abe16a1a22fc7d0d97084f6803b2db9c11
Jay <8x15yz@gmail.com>
2022-08-02 21:09:45 +0900

    maked chatting box

--- frontend/src/views/participate/debateRoomCenterComponent.vue
+++ frontend/src/views/participate/debateRoomCenterComponent.vue
@@ -1,17 +1,55 @@
 <template>
     <div>
-        <div class="debate-moderator">사회자 화면 넣으면 됨</div>
+        <div class="debate-moderator"><div class="debate-moderator-inner" :style="customViewStyle"></div></div>
         <div class="debate-guague">게이지바</div>
         <div class="debate-content">자료화면</div>
     </div>
 </template>
 
+<script>
+export default {
+    computed : {
+        customViewStyle() {
+            return {
+                "--center-video-height" : this.centerVideoHeight,
+                "--center-video-width" : this.centerVideoWidth,
+            }
+        }
+    },
+    data() {
+        return {
+            centerVideoHeight : '',
+            centerVideoWidth : '',
+        }
+    },
+    mounted() {
+        const wValue = document.body.clientWidth*0.75*0.3-20
+        this.centerVideoWidth = `${wValue}px`
+        this.centerVideoHeight = `${wValue*0.6}px`
+
+        window.addEventListener('resize', this.handleResizeHome);
+    },
+    methods: {
+        handleResizeHome() {
+            const wValue = document.body.clientWidth*0.75*0.3-20
+            this.centerVideoWidth = `${wValue}px`
+            this.centerVideoHeight = `${wValue*0.6}px`
+        },
+    }
+}
+</script>
 
 <style>
 .debate-moderator {
-    color: brown;
-    height: 25vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     outline: aqua solid 3px;
+}
+.debate-moderator-inner{
+    background-color: white;
+    height: var(--center-video-height);
+    width: var(--center-video-width);
 }
 .debate-guague {
     color: brown;
--- frontend/src/views/participate/debateRoomSideComponent.vue
+++ frontend/src/views/participate/debateRoomSideComponent.vue
@@ -1,16 +1,81 @@
 <template>
     <div class="debate-room-component-content-side">
-        <div class="panel-video">여기에 패널 화면 넣으면 됨</div>
+        <!-- <div>여기에 패널 화면 넣으면 됨</div> -->
+        <div class="blank-space-video" :style="customViewStyle"></div>
+        <div class="panel-video" :style="customViewStyle"><div class="panel-video-inner"></div></div>
+        <div class="panel-video" :style="customViewStyle"><div class="panel-video-inner"></div></div>
     </div>
 </template>
+
+<script>
+export default {
+    computed : {
+        customViewStyle() {
+            return {
+                "--side-video-width" : this.sideVideoWidth,
+                "--side-video-height" : this.sideVideoHeight,
+                "--side-video-height-inner" : this.sideVideoHeightInner,
+                "--blank-space-video" : this.blankSpaceVideo,
+            }
+        }
+    },
+    data() {
+        return {
+            sideVideoWidth: '',
+            sideVideoHeight: '',
+            sideVideoHeightInner: '',
+            blankSpaceVideo: ''
+        }
+    },
+    mounted() {
+        const wValue = document.body.clientWidth*0.75*0.3-20
+
+        const hBlankValue = document.body.clientHeight*0.8*0.2
+        const hValue = document.body.clientHeight*0.8*0.8*0.5
+
+        this.sideVideoWidth = `${wValue}px`
+        this.sideVideoHeight = `${wValue*0.6}px`
+        this.sideVideoHeightInner = `${hValue}px`
+        this.blankSpaceVideo = `${hBlankValue}px`
+
+        window.addEventListener('resize', this.handleResizeHome);
+    },
+    methods: {
+        handleResizeHome() {
+            const wValue = document.body.clientWidth*0.75*0.3-20
+
+            const hBlankValue = document.body.clientHeight*0.8*0.2
+            const hValue = document.body.clientHeight*0.8*0.8*0.5
+
+            this.sideVideoWidth = `${wValue}px`
+            this.sideVideoHeight = `${wValue*0.6}px`
+            this.sideVideoHeightInner = `${hValue}px`
+            this.blankSpaceVideo = `${hBlankValue}px`
+        },
+    }
+}
+</script>
 
 <style>
 
 .debate-room-component-content-side {
     color: brown;
 }
+.blank-space-video {
+    height: var(--blank-space-video);
+    background-color: blue;
+}
 .panel-video {
-    
+    width: var(--side-video-width);
+    height: var(--side-video-height-inner);
+    background-color: rgb(100, 232, 255);
+    display: flex;
+    align-items: center;
+}
+.panel-video-inner {
+    width: var(--side-video-width);
+    height: var(--side-video-height);
+    background-color: white;
 }
 </style>
 
