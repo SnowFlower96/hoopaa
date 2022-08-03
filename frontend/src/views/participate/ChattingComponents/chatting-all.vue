4b03cf5a7a0dcb5c00f3102cbd4991df622b0a7e
Jay <8x15yz@gmail.com>
2022-08-03 11:30:51 +0900

    채팅창 구성중

--- frontend/src/views/participate/ChattingComponents/chatting-all.vue
+++ frontend/src/views/participate/ChattingComponents/chatting-all.vue
@@ -1,3 +1,14 @@
 <template>
-    
+    <div>
+        <div class="chatting-all">
+            <h1>전체 채팅창</h1>
+            
+        </div>
+    </div>
 </template>
+
+<style>
+.chatting-all {
+    padding: 5%;
+}
+</style>
