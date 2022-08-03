5c7b6909ad7f6bdbbbbe69fd35a3c6d9579366a6
jhno96 <53774255+SnowFlower96@users.noreply.github.com>
2022-08-03 15:51:47 +0900

    Modify backend folder name

--- backend-java/src/main/java/com/ssafy/db/repository/RoomInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.db.repository;
-
-import com.ssafy.db.entity.RoomInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomInfoRepository extends JpaRepository<RoomInfo, Long> {
-
-}
