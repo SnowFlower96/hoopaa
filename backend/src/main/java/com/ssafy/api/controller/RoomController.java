3047b49bf2118a1a481069b9c4b5c96ed0c47a45
jhno96 <53774255+SnowFlower96@users.noreply.github.com>
2022-08-04 17:21:47 +0900

    Add openvidu.fetch() in constructor

--- backend/src/main/java/com/ssafy/api/controller/RoomController.java
+++ backend/src/main/java/com/ssafy/api/controller/RoomController.java
@@ -52,7 +52,7 @@ public class RoomController {
         this.SECRET = secret;
         this.OPENVIDU_URL = openviduUrl;
         this.openVidu = new OpenVidu(OPENVIDU_URL, SECRET);
-
+        this.openVidu.fetch();
         List<Session> sessionList = this.openVidu.getActiveSessions();
         System.out.println(sessionList.size());
         for (Session session : sessionList) {
@@ -185,7 +185,6 @@ public class RoomController {
 
         Session session = this.mapSessions.get(userEm).getSession();
         session.close();
-//        roomService.finishRoom(roomCloseReq);
 
         return ResponseEntity.status(200).body(RoomRes.of(200, "Success"));
 
