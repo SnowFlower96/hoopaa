d6a4af9f41d891cd3cde4b0c81c072f15b2884aa
wonjaechoi <wj5295@naver.com>
2022-08-05 17:09:19 +0900

    Modify RoomService.java RoomServiceImpl.java
    feat : 토론 종료 후 room history user history user stat 업데이트

--- backend/src/main/java/com/ssafy/api/service/RoomService.java
+++ backend/src/main/java/com/ssafy/api/service/RoomService.java
@@ -4,6 +4,7 @@ package com.ssafy.api.service;
 import com.ssafy.api.request.RoomCloseReq;
 import com.ssafy.api.request.RoomEnterReq;
 import com.ssafy.api.request.RoomOpenReq;
+import com.ssafy.common.data.VSession;
 import com.ssafy.db.dto.RoomInfoDto;
 import com.ssafy.db.entity.User;
 
@@ -13,14 +14,14 @@ public interface RoomService {
 
     RoomInfoDto createRoom(RoomOpenReq roomOpenInfo);
 
-    boolean enterRoom(Long id) throws NoSuchElementException;
+//    boolean enterRoom(Long id) throws NoSuchElementException;
     RoomInfoDto findRoomByRoomId(Long roomId);
 
     RoomInfoDto findRoomBySessionId(String sessionId);
 
     RoomInfoDto updatePhaseByRoomId(Long id, int phase);
 
-    void finishRoom(RoomCloseReq roomCloseReq);
+    void finishRoom(VSession vSession);
 
 
     int findHashtagId(String nm);
