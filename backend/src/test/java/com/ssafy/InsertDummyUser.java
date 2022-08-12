d35111b933df8deb8d81c5f2b73ed9ce7cfe10fc
jhno96 <53774255+SnowFlower96@users.noreply.github.com>
2022-08-12 16:46:36 +0900

    [S07P12B302-211] Modify nginx

--- backend/src/test/java/com/ssafy/InsertDummyUser.java
+++ backend/src/test/java/com/ssafy/InsertDummyUser.java
@@ -2,17 +2,13 @@ package com.ssafy;
 
 import com.ssafy.api.request.UserRegisterPostReq;
 import com.ssafy.api.service.UserService;
-import com.ssafy.db.entity.User;
-import com.ssafy.db.entity.UserHistory;
-import com.ssafy.db.entity.UserStat;
-import com.ssafy.db.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
-public class UserTest {
+public class InsertDummyUser {
 
     @Autowired
     UserService userService;
